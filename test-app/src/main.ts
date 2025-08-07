589322aed6e18ca1485f4743c0d55da1725a3822
Lucas Setiady <luca.setia@gmail.com>
2025-08-07 13:38:02 +1000

    Components: Updated placement of app-header and where it is called to follow the correct format as a Component Tree

--- test-app/src/main.ts
+++ test-app/src/main.ts
@@ -4,4 +4,3 @@ import { App } from './app/app';
 import { HeaderComponent } from './app/header.component';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
-bootstrapApplication(HeaderComponent, appConfig).catch((err) => console.error(err));
