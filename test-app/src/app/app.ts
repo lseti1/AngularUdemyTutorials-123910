6e8d91eab04647ef24c6331c6a4c2fdf5d3763e3
Lucas Setiady <luca.setia@gmail.com>
2025-08-08 14:35:40 +1000

    Functionality: Outputting conditional content within HTMLs using @if & @else (meaning we can remove setting default values for variables (e.g. setting it to show user 1 instantly, and instead just define the type)) to display messages if condition isn't met and then display content when condition is true

--- test-app/src/app/app.ts
+++ test-app/src/app/app.ts
@@ -15,7 +15,7 @@ export class App {
   protected readonly title = signal('test-app');
   users = DUMMY_USERS
 
-  selectedUserId = "u1";
+  selectedUserId?: string;
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId);
