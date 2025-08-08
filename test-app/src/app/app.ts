a5b30915cdd58722b1a0faf57bf1ad294cad22ca
Lucas Setiady <luca.setia@gmail.com>
2025-08-08 13:32:02 +1000

    Functionality/Styling: Update app style and created a Configurable Component (Tasks)

--- test-app/src/app/app.ts
+++ test-app/src/app/app.ts
@@ -3,19 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { User } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, User],
+  imports: [RouterOutlet, HeaderComponent, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('test-app');
-
   users = DUMMY_USERS
 
+  selectedUserId = "u1";
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
   onSelectUser(id: string) {
-    console.log('Selected user ID:', id);
+    this.selectedUserId = id;
   }
 }
