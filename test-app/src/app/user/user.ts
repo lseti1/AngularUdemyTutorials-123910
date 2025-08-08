a7dd7d6831566a192179bb892a16342a28d46d17
Lucas Setiady <luca.setia@gmail.com>
2025-08-08 13:00:28 +1000

    Functionality: Adding Typing to EventEmitters (Good idea when the data emitted from this event connects to a function that has clearly defined what type of input it's expecting e.g. <string>)

--- test-app/src/app/user/user.ts
+++ test-app/src/app/user/user.ts
@@ -11,7 +11,7 @@ export class User {
   @Input({required: true}) id!: string;
   @Input({required: true}) avatar!: string;
   @Input({required: true}) name!: string; 
-  @Output() select = new EventEmitter();
+  @Output() select = new EventEmitter<string>();
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
