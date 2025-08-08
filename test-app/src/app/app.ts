ba8fbab3440d0014544eded578944bd8d98fa4fe
Lucas Setiady <luca.setia@gmail.com>
2025-08-08 11:11:00 +1000

    Functionality: Utilising Component Inputs using @Input to take values in externally from another component (e.g. using @Inputs within 'user' components, and getting those values from the app components)

--- test-app/src/app/app.ts
+++ test-app/src/app/app.ts
@@ -2,6 +2,7 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { User } from './user/user';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +12,6 @@ import { User } from './user/user';
 })
 export class App {
   protected readonly title = signal('test-app');
+
+  users = DUMMY_USERS
 }
